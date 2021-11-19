222990e7f7eca1670e9f5c84cc0d543c3893878e
Roman Voronin <r.voronin@mobidev.biz>
2021-11-19 11:44:10 +0200

    blank structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './main/header/header.component';
+import { FooterComponent } from './main/footer/footer.component';
+import { HomePageComponent } from './main/home-page/home-page.component';
+import { ResultPageComponent } from './main/result-page/result-page.component';
+import { SearchComponent } from './main/home-page/search/search.component';
+import { SideCategoriesComponent } from './main/result-page/side-categories/side-categories.component';
+import { ResultContainerComponent } from './main/result-page/result-container/result-container.component';
+import { ResultItemComponent } from './main/result-page/result-item/result-item.component';
+import { PaginationComponent } from './main/result-page/pagination/pagination.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    ResultPageComponent,
+    SearchComponent,
+    SideCategoriesComponent,
+    ResultContainerComponent,
+    ResultItemComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main/result-page/side-categories/side-categories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-categories',
+  templateUrl: './side-categories.component.html',
+  styleUrls: ['./side-categories.component.scss']
+})
+export class SideCategoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
