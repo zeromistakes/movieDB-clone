2f09162026417984d528ffe244edf2723e9d2b79
Roman Voronin <r.voronin@mobidev.biz>
2021-12-03 11:55:12 +0200

    finished pagination

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -13,8 +13,6 @@ export class SearchService {
 
   searchQuery = '';
   currentPage = 1;
-  prevPage = this.currentPage - 1;
-  nextPage = this.currentPage + 1;
   totalPages = 0;
   totalResults = 0;
   constructor(private http: HttpClient) {
@@ -22,7 +20,6 @@ export class SearchService {
 
   getSearchData(query: string) {
     this.searchQuery = query;
-    // this.currentPage = page;
     this.currentPage = 1;
     this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=e318a7a565092a3d0c94c77304aec86f&page=${this.currentPage}&query=${this.searchQuery}`)
       .subscribe((data:any) => {
@@ -41,48 +38,48 @@ export class SearchService {
       })
   }
 
-  incrementPage() {
-    if (this.currentPage < this.totalPages) {
-      this.currentPage++;
-      this.nextPage = this.currentPage + 1;
-      this.prevPage = this.currentPage - 1;
-      this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=e318a7a565092a3d0c94c77304aec86f&page=${this.currentPage}&query=${this.searchQuery}`)
-        .subscribe((data:any) => {
-          console.log(data);
-          this.resultData.next(data);
-        });
-    }
-  }
-  decrementPage() {
-    if (this.currentPage > 1) {
-      this.currentPage--;
-      this.prevPage = this.currentPage - 1;
-      this.nextPage = this.currentPage + 1;
-      this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=e318a7a565092a3d0c94c77304aec86f&page=${this.currentPage}&query=${this.searchQuery}`)
-        .subscribe((data:any) => {
-          console.log(data);
-          this.resultData.next(data);
-        });
-    }
-  }
+  // incrementPage() {
+  //   if (this.currentPage < this.totalPages) {
+  //     this.currentPage++;
+  //     this.nextPage = this.currentPage + 1;
+  //     this.prevPage = this.currentPage - 1;
+  //     this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=e318a7a565092a3d0c94c77304aec86f&page=${this.currentPage}&query=${this.searchQuery}`)
+  //       .subscribe((data:any) => {
+  //         console.log(data);
+  //         this.resultData.next(data);
+  //       });
+  //   }
+  // }
+  // decrementPage() {
+  //   if (this.currentPage > 1) {
+  //     this.currentPage--;
+  //     this.prevPage = this.currentPage - 1;
+  //     this.nextPage = this.currentPage + 1;
+  //     this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=e318a7a565092a3d0c94c77304aec86f&page=${this.currentPage}&query=${this.searchQuery}`)
+  //       .subscribe((data:any) => {
+  //         console.log(data);
+  //         this.resultData.next(data);
+  //       });
+  //   }
+  // }
 
-  goToFirstPage() {
-    this.currentPage = 1;
-    this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=e318a7a565092a3d0c94c77304aec86f&page=${this.currentPage}&query=${this.searchQuery}`)
-      .subscribe((data:any) => {
-        console.log(data);
-        this.resultData.next(data);
-      });
-  }
-
-  goToLastPage() {
-    this.currentPage = this.totalPages;
-    this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=e318a7a565092a3d0c94c77304aec86f&page=${this.currentPage}&query=${this.searchQuery}`)
-      .subscribe((data:any) => {
-        console.log(data);
-        this.resultData.next(data);
-      });
-  }
+  // goToFirstPage() {
+  //   this.currentPage = 1;
+  //   this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=e318a7a565092a3d0c94c77304aec86f&page=${this.currentPage}&query=${this.searchQuery}`)
+  //     .subscribe((data:any) => {
+  //       console.log(data);
+  //       this.resultData.next(data);
+  //     });
+  // }
+  //
+  // goToLastPage() {
+  //   this.currentPage = this.totalPages;
+  //   this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=e318a7a565092a3d0c94c77304aec86f&page=${this.currentPage}&query=${this.searchQuery}`)
+  //     .subscribe((data:any) => {
+  //       console.log(data);
+  //       this.resultData.next(data);
+  //     });
+  // }
 
   changePage(page:any){
     this.currentPage = page;
@@ -92,11 +89,13 @@ export class SearchService {
         this.resultData.next(data);
       });
   }
-  getNextPage(): number {
-    return this.currentPage + 1;
+
+  getNextPage(pageValue:number): number {
+      return this.currentPage + pageValue;
   }
-  getPrevPage(): number {
-    return this.currentPage - 1;
+
+  getPrevPage(pageValue:number): number {
+    return this.currentPage - pageValue;
   }
 }
 
